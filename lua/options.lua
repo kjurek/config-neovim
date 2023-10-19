276aacb446febbc55ef54d16bb945f9392c48f86
Karol Jurek <kjurek123@gmail.com>
2023-10-19 03:35:14 +0200

    Set tab width to 2

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,8 @@ vim.cmd("colorscheme palenight")
 vim.opt.number = true
 vim.opt.nu = true
 vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
 vim.opt.statusline = "%<%f %h%m%r%=%-14.(%l,%c%V%) %P"
